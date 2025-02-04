7b91e78fcf8d223eb3f37d9dd8f6dd13222f1957
Matheus Ribeiro Menezes <matheusmenezzes10@gmail.com>
2025-02-04 15:26:10 -0300

    insere ações

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import Barra from "./Barra";
 function App() {
   return (
     <div className="tela">
-      <div className="containerJogador">
+      <div className="containerJogador1">
         <img src="../src/assets/pikachu-seeklogo.svg" width={200}/>
         <Barra />
       </div>
-      <div className="containerJogador">
+      <div className="containerJogador2">
         <img src="../src/assets/charizard-seeklogo.png" width={300}/>
         <Barra />
       </div>
--- src/Barra.tsx
+++ src/Barra.tsx
@@ -2,19 +2,32 @@ import "./Barra.css";
 
 export default function Barra() {
   return (
-    <div className="container">
-      <div className="pv">
-        <h1>PV</h1>
-        <div className="barraContainer">
-          <span className="barra">100/100</span>
+    <>
+      <div className="container">
+        <div className="status">
+          <div className="pv">
+            <h1>PV</h1>
+            <div className="barraContainer">
+              <span className="barra">100/100</span>
+            </div>
+          </div>
+          <div className="pf">
+            <h1>PF</h1>
+            <div className="barraContainer">
+              <span className="barraPF">100/100</span>
+            </div>
+          </div>
+        </div>
+        <div className="acoes">
+          <h1>Ações</h1>
+          <div className="containerBotoes">
+            <button className="botao">Attack</button>
+            <button className="botao">Cura</button>
+            <button className="botao">Raio</button>
+            <button className="botao">Fogo</button>
+          </div>
         </div>
       </div>
-      <div className="pf">
-        <h1>PF</h1>
-        <div className="barraContainer">
-          <span className="barraPF">100/100</span>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
