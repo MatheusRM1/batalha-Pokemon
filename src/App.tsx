f1c3c061f149f48f715315b1a82e36502587775b
Matheus Ribeiro Menezes <matheusmenezzes10@gmail.com>
2025-02-05 11:41:28 -0300

    estado de ataque básico

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 import Barra from "./Barra";
 
 function App() {
+
+  const [pvP1, setPvP1] = useState(100);
+  const [pfP1, setPfP1] = useState(100);
+  const [pvP2, setPvP2] = useState(100);
+  const [pfP2, setPfP2] = useState(100);
+
+  function atacar(atacante: "P1" | "P2") {
+    if (atacante === "P1" && pfP1 >= 10) {
+      setPvP2((prevPv) => Math.max(prevPv - 20, 0));
+      setPfP1((prevPf) => Math.max(prevPf - 10, 0)); 
+    } else if (atacante === "P2" && pfP2 >= 10) {
+      setPvP1((prevPv) => Math.max(prevPv - 20, 0)); 
+      setPfP2((prevPf) => Math.max(prevPf - 10, 0)); 
+    }
+  }
+
   return (
     <div className="tela">
       <div className="containerJogador1">
         <img src="../src/assets/pikachu-seeklogo.svg" width={200}/>
-        <Barra />
+        <Barra pv={pvP1} pf={pfP1} atacar={() => atacar("P1")} />
       </div>
       <div className="containerJogador2">
         <img src="../src/assets/charizard-seeklogo.png" width={300}/>
-        <Barra />
+        <Barra pv={pvP2} pf={pfP2} atacar={() => atacar("P2")} />
       </div>
     </div>
   );
--- src/Barra.tsx
+++ src/Barra.tsx
@@ -1,6 +1,12 @@
 import "./Barra.css";
 
-export default function Barra() {
+interface BarraProps{
+  pv: number;
+  pf: number;
+  atacar: () => void;
+}
+
+export default function Barra({pv, pf, atacar} : BarraProps) {
   return (
     <>
       <div className="container">
@@ -8,20 +14,20 @@ export default function Barra() {
           <div className="pv">
             <h1>PV</h1>
             <div className="barraContainer">
-              <span className="barra">100/100</span>
+              <span className="barra" style={{width: `${pv}%`}}>{pv}/100</span>
             </div>
           </div>
           <div className="pf">
             <h1>PF</h1>
             <div className="barraContainer">
-              <span className="barraPF">100/100</span>
+              <span className="barraPF" style={{width: `${pf}%`}}>{pf}/100</span>
             </div>
           </div>
         </div>
         <div className="acoes">
           <h1>Ações</h1>
           <div className="containerBotoes">
-            <button className="botao">Attack</button>
+            <button className="botao" onClick={atacar}>Atacar</button>
             <button className="botao">Cura</button>
             <button className="botao">Raio</button>
             <button className="botao">Fogo</button>
