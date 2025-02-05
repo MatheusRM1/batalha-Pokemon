d199add8708159b85398c73db9378cddfbf42e98
Matheus Ribeiro Menezes <matheusmenezzes10@gmail.com>
2025-02-05 14:41:22 -0300

    insere turnos

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,67 @@ import "./App.css";
 import Barra from "./Barra";
 
 function App() {
-
   const [pvP1, setPvP1] = useState(100);
   const [pfP1, setPfP1] = useState(100);
   const [pvP2, setPvP2] = useState(100);
   const [pfP2, setPfP2] = useState(100);
+  const [turno, setTurno] = useState(0);
 
   function atacar(atacante: "P1" | "P2") {
-    if (atacante === "P1" && pfP1 >= 10) {
+    if (atacante === "P1" && pfP1 >= 10 && turno % 2 !== 0 && pvP1 > 0) {
       setPvP2((prevPv) => Math.max(prevPv - 20, 0));
-      setPfP1((prevPf) => Math.max(prevPf - 10, 0)); 
-    } else if (atacante === "P2" && pfP2 >= 10) {
-      setPvP1((prevPv) => Math.max(prevPv - 20, 0)); 
-      setPfP2((prevPf) => Math.max(prevPf - 10, 0)); 
+      setPfP1((prevPf) => Math.max(prevPf - 10, 0));
+      setTurno((prevTurno) => Math.max(prevTurno + 1, 0));
+    } else if (
+      atacante === "P2" &&
+      pfP2 >= 10 &&
+      turno % 2 === 0 &&
+      pvP2 > 0
+    ) {
+      setPvP1((prevPv) => Math.max(prevPv - 20, 0));
+      setPfP2((prevPf) => Math.max(prevPf - 10, 0));
+      setTurno((prevTurno) => Math.max(prevTurno + 1, 0));
     }
+  }
+
+  function TurnoJ1() {
+    return turno % 2 === 0;
+  }
+
+  function TurnoJ2() {
+    return turno % 2 !== 0;
   }
 
   return (
     <div className="tela">
+      <div className="quadro">
+        {pvP1 <= 0 ? 
+          <p className="rodada">Vitória do Jogador 1</p>
+         : pvP2 <= 0 ? 
+          <p className="rodada">Vitória do Jogador 2</p>
+         : turno % 2 === 0 ? 
+          <p className="rodada">Vez do jogador 1</p>
+         : 
+          <p className="rodada">Vez do Jogador 2</p>
+        }
+      </div>
       <div className="containerJogador1">
-        <img src="../src/assets/pikachu-seeklogo.svg" width={200}/>
-        <Barra pv={pvP1} pf={pfP1} atacar={() => atacar("P1")} />
+        <img src="../src/assets/pikachu-seeklogo.svg" width={200} />
+        <Barra
+          pv={pvP1}
+          pf={pfP1}
+          turno={TurnoJ1()}
+          atacar={() => atacar("P1")}
+        />
       </div>
       <div className="containerJogador2">
-        <img src="../src/assets/charizard-seeklogo.png" width={300}/>
-        <Barra pv={pvP2} pf={pfP2} atacar={() => atacar("P2")} />
+        <img src="../src/assets/charizard-seeklogo.png" width={300} />
+        <Barra
+          pv={pvP2}
+          pf={pfP2}
+          turno={TurnoJ2()}
+          atacar={() => atacar("P2")}
+        />
       </div>
     </div>
   );
--- src/Barra.tsx
+++ src/Barra.tsx
@@ -1,33 +1,40 @@
 import "./Barra.css";
 
-interface BarraProps{
+interface BarraProps {
   pv: number;
   pf: number;
+  turno: boolean;
   atacar: () => void;
 }
 
-export default function Barra({pv, pf, atacar} : BarraProps) {
+export default function Barra({ pv, pf, atacar, turno }: BarraProps) {
   return (
     <>
-      <div className="container">
+      <div className="container" style={{ opacity: turno ? 0.5 : 1 }}>
         <div className="status">
           <div className="pv">
-            <h1>PV</h1>
+            <h2>PV</h2>
             <div className="barraContainer">
-              <span className="barra" style={{width: `${pv}%`}}>{pv}/100</span>
+              <span className="barra" style={{ width: `${pv}%` }}>
+                {pv}/100
+              </span>
             </div>
           </div>
           <div className="pf">
-            <h1>PF</h1>
+            <h2>PF</h2>
             <div className="barraContainer">
-              <span className="barraPF" style={{width: `${pf}%`}}>{pf}/100</span>
+              <span className="barraPF" style={{ width: `${pf}%` }}>
+                {pf}/100
+              </span>
             </div>
           </div>
         </div>
         <div className="acoes">
-          <h1>Ações</h1>
+          <h2>Ações</h2>
           <div className="containerBotoes">
-            <button className="botao" onClick={atacar}>Atacar</button>
+            <button className="botao" onClick={atacar}>
+              Atacar
+            </button>
             <button className="botao">Cura</button>
             <button className="botao">Raio</button>
             <button className="botao">Fogo</button>
