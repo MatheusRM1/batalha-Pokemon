dd437f0631b19b7a386444fa6f3736d97f5ee849
Matheus Ribeiro Menezes <matheusmenezzes10@gmail.com>
2025-02-06 14:20:48 -0300

    implementa mais conjuntos de ataque

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,32 @@ import "./App.css";
 import Barra from "./Barra";
 
 function App() {
-  const [pvP1, setPvP1] = useState(100);
-  const [pfP1, setPfP1] = useState(100);
-  const [pvP2, setPvP2] = useState(100);
-  const [pfP2, setPfP2] = useState(100);
+  const [pvJ1, setPvJ1] = useState(100);
+  const [ppJ1, setPpJ1] = useState(100);
+  const [pvJ2, setPvJ2] = useState(100);
+  const [ppJ2, setPpJ2] = useState(100);
   const [turno, setTurno] = useState(0);
 
-  function atacar(atacante: "P1" | "P2") {
-    if (atacante === "P1" && pfP1 >= 10 && turno % 2 !== 0 && pvP1 > 0) {
-      setPvP2((prevPv) => Math.max(prevPv - 20, 0));
-      setPfP1((prevPf) => Math.max(prevPf - 10, 0));
+  const ataquesJ1 = [
+    { nome: "Garra de Dragão", dano: 5, pp: 10 },
+    { nome: "Rajada de Fogo", dano: 30, pp: 25 },
+    { nome: "Golpe de Ar", dano: 10, pp: 15 },
+  ];
+
+  const ataquesJ2 = [
+    { nome: "Thunderstorm", dano: 15, pp: 10 },
+    { nome: "Electro Ball", dano: 20, pp: 30 },
+    { nome: "Ataque Rápido", dano: 5, pp: 8 },
+  ];
+
+  function atacar(atacante: "J1" | "J2", dano: number, pp: number) {
+    if (atacante === "J1" && ppJ1 >= 10 && turno % 2 !== 0 && pvJ1 > 0 && pvJ2 > 0) {
+      setPvJ2((prevPv) => Math.max(prevPv - dano, 0));
+      setPpJ1((prevPp) => Math.max(prevPp - pp, 0));
       setTurno((prevTurno) => Math.max(prevTurno + 1, 0));
-    } else if (
-      atacante === "P2" &&
-      pfP2 >= 10 &&
-      turno % 2 === 0 &&
-      pvP2 > 0
-    ) {
-      setPvP1((prevPv) => Math.max(prevPv - 20, 0));
-      setPfP2((prevPf) => Math.max(prevPf - 10, 0));
+    } else if (atacante === "J2" && ppJ2 >= 10 && turno % 2 === 0 && pvJ1 > 0 && pvJ2 > 0) {
+      setPvJ1((prevPv) => Math.max(prevPv - 20, 0));
+      setPpJ2((prevPp) => Math.max(prevPp - pp, 0));
       setTurno((prevTurno) => Math.max(prevTurno + 1, 0));
     }
   }
@@ -37,32 +44,34 @@ function App() {
   return (
     <div className="tela">
       <div className="quadro">
-        {pvP1 <= 0 ? 
+        {pvJ1 <= 0 ? (
           <p className="rodada">Vitória do Jogador 1</p>
-         : pvP2 <= 0 ? 
+        ) : pvJ2 <= 0 ? (
           <p className="rodada">Vitória do Jogador 2</p>
-         : turno % 2 === 0 ? 
+        ) : turno % 2 === 0 ? (
           <p className="rodada">Vez do jogador 1</p>
-         : 
+        ) : (
           <p className="rodada">Vez do Jogador 2</p>
-        }
+        )}
       </div>
       <div className="containerJogador1">
-        <img src="../src/assets/pikachu-seeklogo.svg" width={200} />
+        <img src="../src/assets/charizard-seeklogo.png" width={300} />
         <Barra
-          pv={pvP1}
-          pf={pfP1}
+          pv={pvJ1}
+          pp={ppJ1}
           turno={TurnoJ1()}
-          atacar={() => atacar("P1")}
+          ataques={ataquesJ1}
+          atacar={(dano, pp) => atacar("J1", dano, pp)}
         />
       </div>
       <div className="containerJogador2">
-        <img src="../src/assets/charizard-seeklogo.png" width={300} />
+        <img src="../src/assets/pikachu-seeklogo.svg" width={300} />
         <Barra
-          pv={pvP2}
-          pf={pfP2}
+          pv={pvJ2}
+          pp={ppJ2}
           turno={TurnoJ2()}
-          atacar={() => atacar("P2")}
+          ataques={ataquesJ2}
+          atacar={(dano, pp) => atacar("J2", dano, pp)}
         />
       </div>
     </div>
--- src/Barra.tsx
+++ src/Barra.tsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import "./Barra.css";
 
 interface BarraProps {
   pv: number;
-  pf: number;
+  pp: number;
   turno: boolean;
-  atacar: () => void;
+  ataques: { nome: string; dano: number; pp: number }[];
+  atacar: (dano: number, pp: number) => void;
 }
 
-export default function Barra({ pv, pf, atacar, turno }: BarraProps) {
+export default function Barra({ pv, pp, ataques, atacar, turno }: BarraProps) {
+  const [opcoesAtaque, setOpcoesAtaque] = useState(false);
+
   return (
     <>
       <div className="container" style={{ opacity: turno ? 0.5 : 1 }}>
@@ -21,10 +25,10 @@ export default function Barra({ pv, pf, atacar, turno }: BarraProps) {
             </div>
           </div>
           <div className="pf">
-            <h2>PF</h2>
+            <h2>PP</h2>
             <div className="barraContainer">
-              <span className="barraPF" style={{ width: `${pf}%` }}>
-                {pf}/100
+              <span className="barraPP" style={{ width: `${pp}%` }}>
+                {pp}/100
               </span>
             </div>
           </div>
@@ -32,12 +36,39 @@ export default function Barra({ pv, pf, atacar, turno }: BarraProps) {
         <div className="acoes">
           <h2>Ações</h2>
           <div className="containerBotoes">
-            <button className="botao" onClick={atacar}>
-              Atacar
-            </button>
-            <button className="botao">Cura</button>
-            <button className="botao">Raio</button>
-            <button className="botao">Fogo</button>
+            {opcoesAtaque ? (
+              <>
+                {ataques.map((ataque, index) => (
+                  <button
+                    key={index}
+                    className="botao"
+                    onClick={() => {
+                      atacar(ataque.dano, ataque.pp);
+                      setOpcoesAtaque(false);
+                    }}
+                  >
+                    {ataque.nome}
+                    <div className="containerInfo">
+                      <p className="info">PP - {ataque.pp}</p>
+                      <p className="info">ATK - {ataque.dano}</p>
+                    </div>
+                  </button>
+                ))}
+                <button
+                  className="botao"
+                  onClick={() => setOpcoesAtaque(false)}
+                >
+                  Voltar
+                </button>
+              </>
+            ) : (
+              <button
+                className="botao"
+                onClick={() => setOpcoesAtaque(true)}
+              >
+                Atacar
+              </button>
+            )}
           </div>
         </div>
       </div>
